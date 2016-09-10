8cfa27f26d24f040566469fb57b4c0b4c260513a
Victory James Ugwudike <javik@MacBook-Pro.local>
2016-09-10 22:34:52 +0100

    Gulp file updated with babelify and autoprefixer. Sass compiler changed to libsass

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,89 @@
 'use strict';
-
-// Load common modules
-// Browser Sync, Sass, Webpack
-// Requirements: 
-// gulp, babel-core, babel-loader, browser-sync, babel-preset-es2015, gulp-babel
-// 
+ 
+// Dependencies
 const gulp          = require('gulp');
 const browserSync   = require('browser-sync').create();
-var compass         = require('gulp-compass');
-var webpack         = require('webpack-stream');
-var kit             = require('gulp-kit');
+const sourcemaps    = require('gulp-sourcemaps');
+const babel         = require('gulp-babel');
+const sass          = require('gulp-sass');
+const kit           = require('gulp-kit');
+const uglify        = require('gulp-uglify-cli');
+const browserify    = require('browserify');
+const source        = require('vinyl-source-stream');
+const buffer        = require('vinyl-buffer');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+// Settings
+const settings = {
+    sass_folder    :   "assets/sass",
+    js_folder      :   "assets/js",
+    css_folder     :   "assets/css",
+    image_folder   :   "assets/img",
+    kit_folder     :   "src/kit",
+    compass_config_file : "./config.rb"
+};
+
+// Sass options
+const sassOptions = {
+    errLogToConsole : true,
+    outputStyle: 'expanded' // Change to compress or compact for production
+}
 
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass','babel','kit'], function() {
+gulp.task('serve', ['sass','babelify', 'kit'], function() {
 
     browserSync.init({
         server: "./"
     });
 
     // Watch changes in Sass files and apply transformation
-    gulp.watch("assets/sass/**/*.scss", ['sass']);
+    gulp.watch(settings.sass_folder+"/**/*.scss", ['sass']);
 
-    // Watch changes in javascript files and apply transpilation to ES5
-    gulp.watch("assets/js/src/**/*.js", ['babel']);
+    // Watch changes in javascript files and apply Babel, and Browserify transpilation to ES5
+    gulp.watch(settings.js_folder+"/src/**/*.js", ['babelify']);
 
-    // Reload browser when a change is observed on javascript files
-    gulp.watch("assets/js/dist/**/*.js").on('change', browserSync.reload);
+    // Reload browser when a change is observed on javascript files in the javascript distribution folder
+    gulp.watch(settings.js_folder+"/dist/**/*.js").on('change', browserSync.reload);
 
     // Watch changes in Kit files and apply transformation to html
-    gulp.watch("src/kit/**/*.kit", ['kit']);
+    gulp.watch(settings.kit_folder+"/**/*.kit", ['kit']);
 
     // Reload browser when a change is observed on html files
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+
+// Compile sass into CSS files
 gulp.task('sass', function() {
-    return gulp.src("assets/sass/**/*.scss")
-        .pipe(compass({
-            config_file: './config.rb',
-            css: 'assets/css',
-            sass: 'assets/sass',
-            image: 'assets/img'
-        }).on('error', function(error) {
-              // Would like to catch the error here 
-              console.log(error);
-              this.emit('end');
-            }))
-        .pipe(gulp.dest("assets/css"))
-        .pipe(browserSync.stream());
+    return gulp.src(settings.sass_folder+'/**/*.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass(sassOptions))
+            .on('error', sass.logError)
+            .pipe(sourcemaps.write())
+            .pipe(autoprefixer())
+            .pipe(gulp.dest(settings.css_folder));
 });
 
-// Use webpack to process ES6 source codes
-gulp.task('babel', function() {
-    return gulp.src('assets/js/src/**/*.js')
-        .pipe(webpack({
-            module: {
-                loaders: [
-                    {
-                        test: /\.js$/,
-                          exclude: /(node_modules|bower_components)/,
-                          loader: 'babel',
-                          query: {
-                            presets: ['es2015']
-                          }
-                      }
-                ]
-            },
-            output: {
-                filename: "app.js"
-            },
-            devtool: "inline-source-map"
-        }).on('error', function(error){
-            // Would like to catch the error here 
-            console.log(error);
-            this.emit('end');
-        }))
-        .pipe(gulp.dest('assets/js/dist/'));
+// Transpile ES6 files and concat ES6 require files
+gulp.task('babelify', function() {
+    return browserify({entries: settings.js_folder+'/src/app.js', debug: true})
+            .transform('babelify', {presets: ["es2015"], sourceMapsAbsolute: true})
+            // Minify output
+            .plugin('minifyify', {map: 'app.min.js.map', output: settings.js_folder+'/dist/app.min.js.map'})
+            .bundle()
+            .on('error', function(err) {
+                console.error(err);
+            })
+            .pipe(source('app.min.js'))
+            .pipe(buffer())
+            .pipe(gulp.dest(settings.js_folder+'/dist/'));
 });
 
-// Kit
+// Kit Gulp Task
 gulp.task('kit', function() {
-    return gulp.src('src/kit/**/*.kit')
+    return gulp.src(settings.kit_folder+'/**/*.kit')
     .pipe(kit())
     .pipe(gulp.dest('.'));
 });
